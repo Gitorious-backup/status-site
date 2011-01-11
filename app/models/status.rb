7c445771e526f015b6fc6e9d6ef14cbefe5046ec
Christian Johansen <christian@shortcut.no>
2011-01-11 17:17:00 +0100

    Verify that status code is valid

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,10 +1,17 @@
 class Status < ActiveRecord::Base
   OK = 1
+  SORTA = 2
+  DOWN = 3
   validates_presence_of :title, :status
 
   def self.current
     status = last
     raise ActiveRecord::RecordNotFound.new if status.nil?
     status
+  end
+
+  def status=(code)
+    return self[:status] = code if [OK, SORTA, DOWN].include?(code)
+    false
   end
 end
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -5,6 +5,38 @@ class StatusTest < ActiveSupport::TestCase
     should "have OK status" do
       assert_not_nil Status::OK
     end
+
+    should "not allow invalid codes" do
+      status = Status.new :title => "OK", :body => "Yes", :status => 0
+
+      assert_no_difference "Status.count" do
+        status.save
+      end
+    end
+
+    should "allow OK" do
+      status = Status.new :title => "OK", :body => "Yes", :status => Status::OK
+
+      assert_difference "Status.count" do
+        status.save
+      end
+    end
+
+    should "allow SORTA" do
+      status = Status.new :title => "OK", :body => "Yes", :status => Status::SORTA
+
+      assert_difference "Status.count" do
+        status.save
+      end
+    end
+
+    should "allow DOWN" do
+      status = Status.new :title => "OK", :body => "Yes", :status => Status::DOWN
+
+      assert_difference "Status.count" do
+        status.save
+      end
+    end
   end
 
   should validate_presence_of :title
